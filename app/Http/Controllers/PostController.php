25ede9a8124e266b59573012d66048406fbf7da6
jphiri <jphiri@myune.edu.au>
2024-07-23 22:09:39 +1000

    initial commit! added create post and view post functionality

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts=Post::all();
+
+        return view('Posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('Posts.create');
+    }
+
+    public function show(Post $post)
+    {
+
+        return view('Posts.show', compact('post'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+       
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        Post::create($request->all());
+        return redirect()->route('posts.index');
+    }
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
